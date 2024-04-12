7484ff73078ffad66db25022f82d2fb04c82c458
Natali <127225486+knightalli@users.noreply.github.com>
2024-04-12 23:20:15 +0500

    add ads swiper

---
+++ BotHub/bothub.client/src/components/elements/Ads-Element.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="ads-container">
+    <div class="ads-text">
+      <h3 class="ads-h3">Info-bot</h3>
+      <div class="ads-info">
+        Привет, роботы! Самые актуальные новинки в мире технологий уже здесь!
+        Специально для вас: абсолютно новые алгоритмы машинного обучения, умные дроны.
+      </div>
+      <button class="ads-more">Подробнее</button>
+    </div>
+    <div class="ads-image">
+      <img src="@/images/info-bot.png" alt="Фотография бота" width="400" height="250" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+  .ads-container {
+    width: 100%;
+    min-height: 300px;
+    padding: 0 100px;
+    background-color: #353535;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .ads-text {
+    width: 45%;
+  }
+
+  .ads-h3 {
+    font-size: 40px;
+    color: #EAEAEA;
+    margin-bottom: 10px;
+  }
+  .ads-info {
+    max-width: 450px;
+    font-size: 20px;
+    color: #EAEAEA;
+    margin-bottom: 10px;
+  }
+
+  .ads-more {
+    background-color: #8274D9;
+    color:#282829;
+    width: 150px;
+    height: 40px;
+    border: none;
+    border-radius: 5px;
+    font-size: 14px;
+  }
+</style>
--- BotHub/bothub.client/src/components/pages/Main-Page.vue
+++ BotHub/bothub.client/src/components/pages/Main-Page.vue
@@ -2,14 +2,61 @@
   <div class="search-container">
     <input class="search" placeholder="введите название бота или краткую информацию" type="text">
   </div>
-  <div>Фильтрация</div>
-  <div>Список ботов</div>
+<div class="line"></div>
+  <div class="swiper">
+    <swiper
+        :modules="modules"
+        :slides-per-view="1"
+        :space-between="500"
+        :loop="true"
+        navigation
+        :pagination="{ clickable: true }"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+
+    >
+      <swiper-slide>
+        <AdsElement></AdsElement>
+      </swiper-slide>
+      <swiper-slide><AdsElement></AdsElement></swiper-slide>
+      <swiper-slide><AdsElement></AdsElement></swiper-slide>
+      ...
+    </swiper>
+  </div>
+<!--  <div>Фильтрация</div>-->
+<!--  <div>Список ботов</div>-->
 </template>
 
 <script>
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-export default {
-  components: {}
+import AdsElement from "@/components/elements/Ads-Element.vue";
+
+  export default {
+
+    components: {
+      AdsElement,
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+        modules: [Navigation, Pagination, Scrollbar, A11y],
+      };
+    },
 }
 </script>
 
@@ -21,5 +68,11 @@ export default {
   color: #FFFFFF;
   border: 0;
   padding-left: 15px;
+}
+
+.line {
+  width: 100%;
+  height: 12px;
+  background-color: #D9D9D9;
 }
 </style>
