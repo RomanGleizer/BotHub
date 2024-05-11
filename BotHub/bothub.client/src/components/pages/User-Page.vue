774378e091a6fb27a3dbb0a86e5c7dee8dbbe1f1
Natali <127225486+knightalli@users.noreply.github.com>
2024-05-11 09:57:05 +0500

    add sort in user-page

--- BotHub/bothub.client/src/components/pages/User-Page.vue
+++ BotHub/bothub.client/src/components/pages/User-Page.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="user-cont">
-    ЭТО ЮЗЕР
     <div class="user">
-      <div class="user-avatar"></div>
-      <div class="user-info"></div>
+      <div class="user-avatar">
+        <img alt="Аватар" class="avatar-image" src="@/images/avatar.png" width="150">
+      </div>
+      <div class="user-info">
+        <p>Имя</p>
+        <p>email</p>
+        <p>Зарегистрирован 23.04.2024г.</p>
+        <p>О себе</p>
+      </div>
     </div>
     <div class="bot-list">
-      <div v-for="bot in searchList" :key="bot.id" class="bot-card">
+      <div class="filter">
+        <button :class="{'filter-active': isAdded}" class="filter-button" @click="sortParam='added'">Популярное</button>
+        <button :class="{'filter-active': isLike}" class="filter-button" @click="sortParam='like'">Новинки</button>
+      </div>
+      <div v-for="bot in sortedList" :key="bot.id" class="bot-card">
         <BotCardElement :bot="bot"></BotCardElement>
       </div>
     </div>
@@ -25,19 +35,52 @@ export default {
   data() {
     return {
       botList: BotList,
-      name: 'Ezekiel'
+      name: 'Ezekiel',
+      sortParam: '',
+      isAdded: true,
+      isLike: false
     }
   },
   computed: {
-    searchList() {
-      return this.botList.filter(bot => {
-        return bot.authorName == this.name;
-      })
-    }
+    sortedList() {
+      switch (this.sortParam) {
+        case 'added':
+          return this.botList.filter(bot => {
+            return bot.authorName == this.name;
+          }).sort(this.sortByAdded);
+        case 'like':
+          return this.botList.filter(bot => {
+            return bot.authorName == 'Roman';
+          }).sort(this.sortByLike);
+        default:
+          return this.botList.filter(bot => {
+            return bot.authorName == this.name;
+          }).sort(this.sortByAdded);
+      }
+    },
+  },
+  methods: {
+    sortByAdded() {
+      this.isAdded = true;
+      this.isLike = false;
+    },
+    sortByLike() {
+      this.isAdded = false;
+      this.isLike = true;
+    },
   }
 }
 </script>
 
 <style scoped>
+  .user-cont {
+    width: 80%;
+    margin: 0 auto;
+    height: 100%;
+    background-color: #353535;
+  }
 
+  .avatar-image {
+    border-radius: 50%;
+  }
 </style>
